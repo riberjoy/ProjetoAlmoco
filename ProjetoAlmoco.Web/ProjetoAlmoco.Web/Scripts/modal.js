e3752b2d3e618cce6274c1219aba5d73c485e66b
Joyce <riberjoy@outlook.com>
2020-02-07 12:56:29 -0300

    FRONT002-2020 - Criação de Alert

--- ProjetoAlmoco.Web/ProjetoAlmoco.Web/Scripts/modal.js
+++ ProjetoAlmoco.Web/ProjetoAlmoco.Web/Scripts/modal.js
@@ -7,42 +7,100 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-//btn.onclick = function () {
-//    modal.style.display = "block";
-//}
-
-// When the user clicks on <span> (x), close the modal
-
-
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modall) {
-        modall.style.display = "none";
-    }
-}
+
 
 function insModal(tipo) {
     $("<div></div>", { id: 'myModal', class: 'modal' }).appendTo("div div.conteudo__titulo");
-    $("<div></div>", { class: 'modal-content' }).appendTo("div div div.modal");
+    $("<div></div>", { class: 'modal-content ' }).appendTo("div div div.modal");
     $("<span></span>", { class: 'close' }).appendTo("div div div.modal div.modal-content");
     $("span.close").html("&times;");
-    $("<p>Some text in the Modal..</p>").appendTo("div div div.modal div.modal-content");
 
 
-    $("<button></button>", { class: 'login__cadastro', value:'cadasrtar' }).appendTo("div div div.modal div.modal-content");
+    $("<h1></h1>", { class: 'nomeCategoria' }).appendTo("div div div.modal div.modal-content");
+    $("h1.nomeCategoria").html(tipo.toString());
+    $("h1.nomeCategoria").css({
+        "width": "100%",
+        "text-aling": "center"
+    });
+    
+    $("<input>", { class: "admin__cadCategoria", type: "text", placeholder:"nome", name:"categoria", id:"", autocomplete:"off" }).appendTo("div div div.modal div.modal-content");
+    $("input.admin__cadCategoria").css({
+        "margin-top":" 0px",
+        "width": "80%",
+        "height": "20px",
+        "background-color": "white",
+        "border-bottom": "1px solid black",
+        "border-radius": "0px 0px 0px 0px",
+        "padding": "20px",
+        "font-size": " 15pt",
+        "color": "black",
+        "padding-left": "3%",
+        "padding-bottom": "2%",
+        "transition": "none",
+        "outline": "none",
+    });
 
-
-
+    $("<input>", { class: "admnCadCategoria__link", type: "submit", value: "Adicionar", name: "", id: "" }).appendTo("div div div.modal div.modal-content");
+    $("input.admnCadCategoria__link").css({
+        "margin-top": "25px",
+        "margin-bottom": "25px",
+        "background-color": "#00E866",
+        "color": "white",
+        "font-size": "15pt",
+        "width": "80%",
+        "height": "50px",
+        "border": " none",
+        "border-radius": "30px",
+        "cursor": "pointer",
+        "transition": ".4s",
+        "outline": "none"
+    });
 
     $("span").click(function () {
         $("div.modal").remove();
         $("div.modal-content").remove();
         $("span.close").remove();
-        $("p").remove();
-       // modall.style.display = "none";
+        $("h1.nomeCategoria").remove();
+        $("hr.addCategoria").remove();
+        $("input.admin__cadCategoria").remove();
+        $("input.admnCadCategoria__link").remove();
     });
+
+    $("input.admnCadCategoria__link").click(function () {
+        $("div.modal").remove();
+        $("div.modal-content").remove();
+        $("span.close").remove();
+        $("h1.nomeCategoria").remove();
+        $("hr.addCategoria").remove();
+        $("input.admin__cadCategoria").remove();
+        $("input.admnCadCategoria__link").remove();
+    });
+
+    
 }
+
+window.onclick = function (event) {
+    console.log(event.target.id)
+    if (event.target == myModal) {
+        $("div.modal").remove();
+        $("div.modal-content").remove();
+        $("span.close").remove();
+        $("h1.nomeCategoria").remove();
+        $("hr.addCategoria").remove();
+        $("input.admin__cadCategoria").remove();
+        $("input.admnCadCategoria__link").remove();
+    }
+}
+
+
+
+//<form method="post">
+//    <h1>categoria</h1>
+ //  <hr/>
+//    @Html.EditorFor(model => model.Senha, new {htmlAttributes = new { @class = "login__input", placeholder = "SENHA", autocomplete = "off", type = "password" } })
+ //   <input class="login__link" type="submit" value="LOGAR">
+//</form>
 
 
   //  < !--The Modal-- >

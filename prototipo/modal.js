dd008f2dcbc783c3b6a2458331de6bdf19f1c018
Matheus <matheusaugustosimoes@hotmail.com>
2020-02-12 19:27:32 -0300

    FRONT003-2020 - adpatado , e lista clientes feito

--- prototipo/modal.js
+++ prototipo/modal.js
@@ -11,30 +11,3 @@ function MostraModal(modalId){
         }
     });
 }
-
-// function Obrigatorio(inputId,modalInputBtn){
-//     var input = document.getElementsByClassName("input__addCat").value
-//     var botao = document.getElementsByClassName("modal__input__btn")
-//     if(input ==""){
-//         //rejeitado
-    
-//         botao.prop( "disabled", true );
-
-//     }else{
-        
-//         botao.attr( "disabled", false );
-//     }
-// }
-
-$(document).ready(function () {
-    $('.modal__input__btn').attr('disabled', true);
-    $('input[type="text"],textarea').on('keyup', function () {
-        var textarea_value = $("#texta").val();
-        var text_value = $('input[name="textField"]').val();
-        if (textarea_value != '' && text_value != '') {
-            $('input[type="submit"]').attr('disabled', false);
-        } else {
-            $('input[type="submit"]').attr('disabled', true);
-        }
-    });
-});
